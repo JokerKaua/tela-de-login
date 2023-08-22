77af6e5e04b1f58e16c6872534c9947260d2d5fe
jokerkaua123@gmail.com <jokerkaua123@gmail.com>
2023-08-22 09:50:53 -0300

    Animação span user e pass

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const loginButton =  document.getElementById("login")
 //Adicionando uma função ao botão de login
 loginButton.addEventListener("click", login)
 
+//Animação dos spans
+
+const userSpan = document.getElementsByClassName('usuario');
+const passSpan = document.getElementsByClassName('senha');
+
+spanAct(userSpan); 
+spanAct(passSpan);
+
+function spanAct(elemento){
+  elemento[0].children[1].addEventListener('focus', ()=> {
+     elemento[0].children[0].classList.add('spanact');
+  });
+  elemento[0].children[1].addEventListener('blur', () => {
+    elemento[0].children[0].classList.remove('spanact');
+  });
+  
+}
+
 function login() {
     //Pega os elementos e coloca em variaveis
     const usernameInput = document.getElementById("username");
