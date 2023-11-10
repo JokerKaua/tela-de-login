ef96bc53c96ed9721e45546a1d9ee3a0fd23dd41
name <jokerkaua123@gmail.com>
2023-11-10 15:10:48 -0300

    Servidor e rotas

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
-app.use(express.static('login-page'));    
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/login', express.static('login-page'));    
 
 app.get('/', (req, res) => {
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/login-page/index.html');
 })
+
+app.post('/login', (req, res) => {
+  res.send(`Paramters: ${req.params}`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
